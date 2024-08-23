c3043dd9414aefa07b5db7cff955de9448f8cf7e
fauxir <fauxir@gmail.com>
2024-08-23 18:40:57 +0100

    Removed fs

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 const { searchAll } = require('./lib/search');
 const { getFromRoomId, getFromRoomUrl } = require('./lib/details')
-const fs = require('fs').promises;
+// const fs = require('fs').promises;
 
 async function test0() {
   const roomId = "https://www.airbnb.co.uk/rooms/20669368";
@@ -8,7 +8,7 @@ async function test0() {
   const checkIn = "2024-11-02";
   const checkOut = "2024-11-10";
   const data = await getFromRoomUrl(roomId, currency, checkIn, checkOut, "");
-  await fs.writeFile("details.json", JSON.stringify(data, null, 2));
+  // await fs.writeFile("details.json", JSON.stringify(data, null, 2));
 }
 
 async function test1() {
@@ -17,7 +17,7 @@ async function test1() {
   const checkIn = "2024-11-02";
   const checkOut = "2024-11-10";
   const data = await getFromRoomId(roomId, currency, checkIn, checkOut, "");
-  await fs.writeFile("details.json", JSON.stringify(data, null, 2));
+  // await fs.writeFile("details.json", JSON.stringify(data, null, 2));
 }
 
 async function test2() {
@@ -30,7 +30,7 @@ async function test2() {
   const swLong = -77.59713412765507;
   const zoomValue = 2;
   const results = await searchAll(checkIn, checkOut, neLat, neLong, swLat, swLong, zoomValue, currency, "");
-  await fs.writeFile("search.json", JSON.stringify(results, null, 2));
+  // await fs.writeFile("search.json", JSON.stringify(results, null, 2));
 }
 
 async function main() {
